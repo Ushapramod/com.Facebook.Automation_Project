ee6d1eb067d8cec303daf15f62c5fa2f5a10ff3c
USHA PRAMOD <USHA PRAMOD@DESKTOP-3PMDTNC>
2024-10-05 10:39:59 +0530

    First Commit

---
+++ FaceBookAutomation/src/test/java/Login/LoginPage.java
@@ -0,0 +1,36 @@
+package Login;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        // Set the path for the ChromeDriver executable
+        System.setProperty("webDriver.chrome.driver", "path/to/chromedriver"); // Update this path
+        driver = new ChromeDriver();
+        driver.manage().window().maximize(); // Maximize the browser window
+    }
+
+    @Test
+    public void openFacebook() {
+        // Open Facebook
+        driver.get("https://www.facebook.com");
+        // You can add assertions here to validate the page title or other elements if needed
+        System.out.println("Opened Facebook: " + driver.getTitle());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        // Close the browser
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
